ee066ca5222cecb77f6b58bb802aa5181ce293ea
Andrew Crites <acrites@mobiquityinc.com>
2017-12-05 12:23:50 -0500

    Adding type definitions

---
+++ src/index.d.ts
@@ -0,0 +1,9 @@
+type Case = Symbol;
+export const upper: Case;
+export const lower: Case;
+
+export function spongeMock(str: string): string;
+export function sPoNgEmOCk(str: string): string;
+export function decideCase(curr: Case, prev: string, prev2: string): Case;
+export function isLetter(char: string): boolean;
+export function getCase(char: string): Case;
